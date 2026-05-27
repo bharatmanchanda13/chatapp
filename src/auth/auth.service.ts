5f9fd5d359a2ea9ee7e5829c356df1d28933bea2
Srishta <srishta@Srishtas-MacBook-Pro.local>
2026-05-27 11:33:48 +0530

    update

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
+    constructor(private readonly prisma: PrismaService) {}
+
     async login(dto: LoginDto): Promise<any> {
         
     }
 
     async register(dto: RegisterDto): Promise<any> {
-        console.log(dto,"::dto");
+        const existingUser = await this.prisma.user.findFirst({
+            where: {
+            OR: [
+                { email: dto.email },
+                { phone: dto.phone },
+            ],
+            },
+        });
+        if (existingUser) {
+            throw new BadRequestException(
+                'User already exists',
+            );
+        }
+
+        const hashedPassword = await bcrypt.hash(dto.password, 10);
+        const user = await this.prisma.user.create({
+            data: {
+                name: dto.name,
+                email: dto.email,
+                phone: dto.phone,
+                password: hashedPassword,
+            },
+        });
+        return user;
     }
 }
